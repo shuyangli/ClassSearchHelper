ab172b5b9ed2751c0ec52f09e8ddeada62cc9033
Shuyang Li <shuyang.li.95@gmail.com>
2014-08-24 23:11:02 -0400

    Initial Commit

---
+++ ClassSearchHelperCLT/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ClassSearchHelperCLT
+//
+//  Created by Shuyang Li on 8/24/14.
+//  Copyright (c) 2014 Shuyang Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
